c7e2cb7886d70dbda3f1c5d750560baf38446dfe
katevlaar <kate.vlaar@shaw.ca>
2021-04-27 16:03:03 -0600

    UI updates with search results populated

--- the-shoppies/src/components/SearchBar.js
+++ the-shoppies/src/components/SearchBar.js
@@ -1,20 +1,48 @@
-// import './App.css';
 import React, { useState } from 'react';
+import SearchResultMobile from './SearchResultMobile';
 
 function SearchBar() {
   const [query, setQuery] = useState("");
-  const [searchResults, setSearchResults] = useState([{}]);
+  const [searchResults, setSearchResults] = useState([]);
   
   async function searchBarOnChange(e) {
-    const axios = require('axios');
     if (e.key === 'Enter') {
       setQuery(e.target.value);
-
-      axios.get(`http://www.omdbapi.com/?s=${e.target.value}&apikey=${process.env.REACT_APP_OMDB_API_KEY}`)
-      .then(function (response) {
-        console.log(response);
-      })
+      fetchResultsWithPaging(e.target.value);
     }
+  }
+
+  async function fetchResultsWithPaging(query) {
+    var pageNumber = 1;
+    var searchResultsArray = [];
+
+    const axios = require('axios');
+
+    var response = await axios(`http://www.omdbapi.com/?s=${query}&apikey=${process.env.REACT_APP_OMDB_API_KEY}&page=${pageNumber}`);
+    var totalSearchResults = response.data.totalResults;
+
+    while (totalSearchResults > 0) {
+      if (response.data.Response === "True") {        
+        for (var i = 0; i < response.data.Search.length; i++) {
+          if (response.data) {
+            searchResultsArray.push({
+              title: response.data.Search[i].Title,
+              year: response.data.Search[i].Year,
+            });
+          }
+        }
+        totalSearchResults -= response.data.Search.length;
+        pageNumber += 1;
+        
+        console.log(searchResultsArray);
+        } else {
+          // TODO: Show error message
+          break;
+        }
+      response = await axios(`http://www.omdbapi.com/?s=${query}&apikey=${process.env.REACT_APP_OMDB_API_KEY}&page=${pageNumber}`);
+
+    }
+    setSearchResults(searchResultsArray);
   }
 
   return (
@@ -24,6 +52,9 @@ function SearchBar() {
           <span className="material-icons md-dark">search</span>
         </div>
         {query === "" ? <div></div> : <div><h4>Search results for "{query}"</h4></div> }
+        {searchResults.map(function(o, i) {
+          return <SearchResultMobile title={o.title} year={o.year} key={i}/>
+        })}
     </div>
   );
 }
--- the-shoppies/src/components/SearchResultMobile.js
+++ the-shoppies/src/components/SearchResultMobile.js
@@ -3,7 +3,9 @@ function SearchResultMobile(props) {
 
   return (
     <div className="search-result-mobile">
-        
+      <p className="movie-title-mobile"><b>{props.title}</b></p>
+      <p className="movie-year-mobile"> ({props.year}) </p>
+      <span className="material-icons">star_border</span>
     </div>
   );
 }
